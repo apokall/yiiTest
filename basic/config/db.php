6854a36b16476ea040bd9e4d8f0226533524faf7
apokall <apokall88@gmail.com>
2017-05-23 15:58:49 +0300

    added countries

--- basic/config/db.php
+++ basic/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' => '4rtyuehe3',
     'charset' => 'utf8',
 ];
